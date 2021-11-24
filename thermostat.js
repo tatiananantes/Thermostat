0c39cd686fb86d9b2e5270add0332a640f5f066a
Adam Harbisher <aharbisher@gmail.com>
2021-11-24 17:13:27 +0000

    Added tests and contraints to add and down functions

--- thermostat.js
+++ thermostat.js
@@ -1,24 +1,40 @@
 class Thermostat {
 
-constructor() {
-  this.temp = 20;
-}  
+  constructor() {
+    this.temp = 20;
+    this.maxTemp = 25;
+    this.powerSaving = true;
+  }  
 
-getTemperature() {
-  return this.temp;
+  getTemperature() {
+    return this.temp;
+  }
+
+  up() {
+    if(this.temp < this.maxTemp) {
+      this.temp++;
+    }
+  }
+
+  down() {
+    if(this.temp > 10) {
+      this.temp--;
+    }
+  }
+
+  setPowerSavingMode(boolean) {
+    this.powerSaving = boolean;
+    this.setMaxTemp();
+  }
+
+  setMaxTemp() {
+    if(this.powerSaving) {
+      this.maxTemp = 25;
+    } else {
+      this.maxTemp = 32;
+    }
+  }
+
 }
-
-
-
-
-}
-
-
-
-
-
-
-
-
 
 module.exports = Thermostat;
--- thermostat.test.js
+++ thermostat.test.js
@@ -1,9 +1,28 @@
 const Thermostat = require('./thermostat');
 
-describe('get Temperature', () => {
+describe('Thermostat', () => {
   const thermostat = new Thermostat();
-  it('should return 20', () => {
+  it('get temperature - should return 20', () => {
     expect(thermostat.getTemperature()).toBe(20);
+  });
+
+  it('can have temperature increased', () => {
+    thermostat.up();
+    thermostat.up();
+    expect(thermostat.getTemperature()).toBe(22);
+  });
+
+  it('can have temperature decreased', () => {
+    thermostat.down();
+    expect(thermostat.getTemperature()).toBe(21);
+  });
+
+  it('sets max temp based on powersaving', () => {
+    thermostat.setPowerSavingMode(true);
+    for (let i = 0 ; i < 10 ; i++) {
+      thermostat.up();
+    }
+    expect(thermostat.getTemperature()).toBe(25);
   });
 
 });
