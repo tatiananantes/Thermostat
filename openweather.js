f5607c22e28b04d3fbdec2db68ec903f09c9e577
Tatiana Nantes <tatiananantes@gmail.com>
2021-11-26 11:57:19 +0000

    gets response back by using a callback instead of logging

--- openweather.js
+++ openweather.js
@@ -3,17 +3,17 @@ const got = require('got');
 
 class Weather {
 
-  fetchWeatherData = (city) => {
+  fetchWeatherData = (city, callback) => {
     const apiUrl = `http://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=${apiKey}`;
-    
-    let weatherData = null;
     got(apiUrl).then((response) => {
-    weatherData = JSON.parse(response.body);
-    console.log(weatherData);
+      callback(response.body);
     });
   }
+
 }
 const weather = new Weather();
-weather.fetchWeatherData('London');
+weather.fetchWeatherData('Barcelona', (weatherData) => {
+  console.log(JSON.parse(weatherData));
+})
 
 module.exports = Weather;
